e271277587bbe97903c05659c4aeb6071a08e44c
GrozYeux <66424461+GrozYeux@users.noreply.github.com>
2022-03-24 15:53:56 -0400

    animation taupe spawn

--- Assets/Scripts/Taupe.cs
+++ Assets/Scripts/Taupe.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class Taupe : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Vector3 initialPosition;
+    private float time;
+
+    private void OnEnable()
     {
-        
+        transform.localPosition = initialPosition;
+        time = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(time < 0.1)
+        {
+            transform.localPosition = new Vector3(
+            transform.localPosition.x,
+            transform.localPosition.y + Time.deltaTime * 10,
+            transform.localPosition.z);
+            time += Time.deltaTime;
+        }
     }
+
 
     private void OnTriggerEnter(Collider other)
     {
