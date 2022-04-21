c9c3c72a1928d2353cf1ce024c0bebcbd208b4d9
Iilun <iilun.games@laposte.net>
2022-04-21 14:03:07 -0400

    ButtonSign

---
+++ Assets/ButtonInputSign.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonInputSign : MonoBehaviour
+{
+    
+    [System.Serializable]
+    public struct Entry
+    {
+        public OVRInput.RawButton button;
+        public bool isGreen;
+
+        public GameObject greenImage;
+
+        public GameObject redImage;
+    }
+    [SerializeField] List<Entry> entries;
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(Entry E in entries) {
+            OVRInput.RawButton button = E.button;
+            if (OVRInput.Get(button)) {
+                if(E.isGreen) {
+                    E.greenImage.SetActive(true);
+                    E.redImage.SetActive(false);
+                } else {
+                    E.redImage.SetActive(true);
+                    E.greenImage.SetActive(false);
+                }
+            }
+        }
+    }
+
+    
+}
